4ba9b87b37b142e14a4cec4c1beac47af51734bc
jamil <jrahman88>
2020-03-16 02:53:11 -0400

    Newly added

--- myPandas_Loc.py
+++ myPandas_Loc.py
@@ -13,9 +13,9 @@ df = pd.read_csv("~/Desktop/Learning/Sandbox/PythonSandbox/Data/pokemon_data.csv
 print("\n",df.head(3))
 
 #Extract all rows and selective range of columns using loc() where it takes the column header names
-df1 = df.loc[:, ("HP", "Attack", "Defense")]
+df1 = df.loc[:, ("HP", "Attack", "Defense")] #<-- all rows denoted by ':'
 print("\n",df1.head(10))
 
 #Extract selective range of rows and selective range of columns using iloc()
-df2 = df.loc[0:100,("Defense", "Speed", "Generation", "Legendary")]
+df2 = df.loc[0:100,("Defense", "Speed", "Generation", "Legendary")] #<-- selective range of rows (0:100)
 print("\n",df2.head(50))
