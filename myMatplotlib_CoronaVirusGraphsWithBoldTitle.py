db697845b779845eaad083e5221a75fead4a3e12
jamil <jrahman88>
2020-03-18 05:17:16 -0400

    Newly added

---
+++ myMatplotlib_CoronaVirusGraphsWithBoldTitle.py
@@ -0,0 +1,238 @@
+'''
+Using Matplotlib module of Python, draw a different graphs, data read from (corona_virus.csv)
+Note that, here we do the followings to fine tune the dataframe:
+1. Read the data file corona_virus.csv from file system and create a dataframe
+2. Delete the last row so that 'Total' is not considered in any calculations using: cv = cv.iloc[:-1]
+3. Drop the 'Diamond Princess' row as a country usincv = cv[cv.Country != 'Diamond Princess']g:
+'''
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+import datetime
+
+#This is for how many data will be read
+dataSize = 9
+
+desired_width=360
+pd.set_option('display.width', desired_width)
+pd.set_option('display.max_columns',120)
+
+dt = datetime.datetime.now()
+today = dt.strftime("%A, %d %B %Y, %H:%M:%S")
+todayDate = dt.strftime("%A, %d %B %Y")
+
+def plotGraph(dfname, rowname, colname, fignum, graphcolor, titlestr):
+    x1 = dfname[rowname]
+    y1 = dfname[colname]
+    plt.figure(fignum, figsize=(14, 6))
+    plt.title(titlestr + r"$\bf{" + colname + "}$" + ' - [Total countries effected as of today = {}]'.format(cv.shape[0]))
+    plt.suptitle(today)
+    plt.xlabel("Countries")
+    plt.ylabel(colname)
+    barplot = plt.bar(x1, y1, color=graphcolor)
+    plt.grid(True)
+    for bar in barplot:
+        yval = bar.get_height()
+        plt.text(bar.get_x() + bar.get_width() / 2.0, yval, int(yval), va='bottom')  # va: vertical alignment y positional argument
+    plt.show()
+    return()
+
+#----WORLD DATA----#
+#Read the coronoa_virus.csv from the file system and transform into a DataFrame using read_csv() method
+cv = pd.read_csv("~/Desktop/Learning/Sandbox/PythonSandbox/Data/corona_virus.csv")
+cv = cv.iloc[:-1] #<-- Here we drop the last row of the dataframe (last row holds the total of all rows)
+cv = cv[cv.Country != 'Diamond Princess'] #<-Here we drop a row where the 'Country' column contains a value 'Diamond Princess'
+cv = cv.fillna(0) #<-- Fill all NaN values with 0
+
+#Plot the pie chart for World Data ('Total cases', 'Total recovered', 'Total deaths', 'Active cases')
+s = cv.sum(axis=0) #<--Add all columns and create a series
+totalCase = s['Total cases']
+totalRecovered = s['Total recovered']
+totalActiveCases = s['Active cases']
+totalDeaths = s['Total deaths']
+
+# Pie chart plotting
+labels = 'Total Recovered', 'Active Cases', 'Total Deaths'
+sizes = [totalRecovered, totalActiveCases, totalDeaths]
+explode = (0.0, 0.0, 0.3)  # only "explode" the 3rd slice (i.e. 'Total Deaths')
+
+fig1, ax1 = plt.subplots()
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.f%%',
+        shadow=True, startangle=50)
+ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+ax1.set(aspect="equal", title='Total Global Cases as of {} = {}'.format(todayDate, totalCase))
+plt.show()
+
+#Sorting the data based on a single column in a file and output using sort_values() method (descending order - ascending=False)
+cv_TotalCases = cv.sort_values('Total cases', ascending=False)
+cv_TotalRecovered = cv.sort_values('Total recovered', ascending=False)
+cv_TotalCasesPerMillion = cv.sort_values('Total cases per 1M pop.', ascending=False)
+cv_TotalDeaths = cv.sort_values('Total deaths', ascending=False)
+cv_NewCases = cv.sort_values('New cases', ascending=False)
+cv_NewDeaths = cv.sort_values('New deaths', ascending=False)
+
+#Graph of top 10 'Total cases'
+x = cv_TotalCases.head(10)
+rowcategory = "Country"
+columncategory = "Total cases"
+figureNum = 1
+graphColor = "Red"
+titlestring = "Top 10 countries based on "
+plotGraph(x, rowcategory, columncategory, figureNum, graphColor, titlestring)
+
+#Graph of top 10 'Total recovered'
+x = cv_TotalRecovered.head(10)
+rowcategory = "Country"
+columncategory = "Total recovered"
+figureNum = 2
+graphColor = "limegreen"
+titlestring = "Top 10 countries based on "
+plotGraph(x, rowcategory, columncategory, figureNum, graphColor, titlestring)
+
+#Graph of top 10 'Total cases per 1M pop.'
+x = cv_TotalCasesPerMillion.head(10)
+rowcategory = "Country"
+columncategory = "Total cases per 1M pop."
+figureNum = 3
+graphColor = "cornflowerblue"
+titlestring = "Top 10 countries based on "
+plotGraph(x, rowcategory, columncategory, figureNum, graphColor, titlestring)
+
+#Graph of top 10 'Total deaths'
+x = cv_TotalDeaths.head(10)
+rowcategory = "Country"
+columncategory = "Total deaths"
+figureNum = 4
+graphColor = "dimgray"
+titlestring = "Top 10 countries based on "
+plotGraph(x, rowcategory, columncategory, figureNum, graphColor, titlestring)
+
+#Graph of top 10 'New cases'
+x = cv_NewCases.head(10)
+rowcategory = "Country"
+columncategory = "New cases"
+figureNum = 5
+graphColor = "lightcoral"
+titlestring = "Top 10 countries based on "
+plotGraph(x, rowcategory, columncategory, figureNum, graphColor, titlestring)
+
+#Graph of top 10 'New deaths'
+x = cv_NewDeaths.head(10)
+rowcategory = "Country"
+columncategory = "New deaths"
+figureNum = 6
+graphColor = "dimgrey"
+titlestring = "Top 10 countries based on "
+plotGraph(x, rowcategory, columncategory, figureNum, graphColor, titlestring)
+
+
+#----SAARC DATA----#
+cv1 = pd.read_csv("~/Desktop/Learning/Sandbox/PythonSandbox/Data/corona_virus.csv")
+cv1 = cv1.fillna(0)
+cv1 = cv1.loc[(cv1["Country"] == 'Bangladesh')|
+              (cv1["Country"] == 'Bhutan')|
+              (cv1["Country"] == 'India')|
+              (cv1["Country"] == 'Maldives')|
+              (cv1["Country"] == 'Nepal')|
+              (cv1["Country"] == 'Pakistan')|
+              (cv1["Country"] == 'Sri Lanka')]
+
+# #Plot the pie chart for SAARC countries ('Total cases')
+# #Extract country specific sub-df
+# cv1_Bangladesh = cv1.loc[(cv1["Country"] == 'Bangladesh')]
+# cv1_Bhutan = cv1.loc[(cv1["Country"] == 'Bhutan')]
+# cv1_India = cv1.loc[(cv1["Country"] == 'India')]
+# cv1_Malvives = cv1.loc[(cv1["Country"] == 'Maldives')]
+# cv1_Nepal = cv1.loc[(cv1["Country"] == 'Nepal')]
+# cv1_Pakistan = cv1.loc[(cv1["Country"] == 'Pakistan')]
+# cv1_SriLanka = cv1.loc[(cv1["Country"] == 'Sri Lanka')]
+#
+# #Now extract country specific 'Total cases'
+# tc_Bangladesh = cv1_Bangladesh['Total cases']
+# tc_Bhutan = cv1_Bhutan['Total cases']
+# tc_India = cv1_India['Total cases']
+# tc_Maldives = cv1_Malvives['Total cases']
+# tc_Nepal = cv1_Nepal['Total cases']
+# tc_Pakistan = cv1_Pakistan['Total cases']
+# tc_SriLanka = cv1_SriLanka['Total cases']
+#
+# print(tc_Bangladesh)
+# print(tc_Bhutan)
+# print(tc_India)
+# print(tc_Maldives)
+# print(tc_Nepal)
+# print(tc_Pakistan)
+# print(tc_SriLanka)
+#
+# # Pie chart plotting
+# labels = 'Bangladesh', 'Bhutan', 'India', 'Maldives', 'Nepal', 'Pakistan', 'Sri Lanka'
+# sizes = [tc_Bangladesh, tc_Bhutan, tc_India, tc_Maldives, tc_Nepal, tc_Pakistan, tc_SriLanka]
+# explode = (0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0)
+#
+# fig2, ax2 = plt.subplots()
+# ax2.pie(sizes, explode=explode, labels=labels, autopct='%.0f%%', shadow=True, startangle=90)
+# ax2.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+# ax2.set(aspect="equal", title='SAARC Picture')
+# plt.show()
+
+#Sorting the data based on a single column in a file and output using sort_values() method (descending order - ascending=False)
+cv1_TotalCases = cv1.sort_values('Total cases', ascending=False)
+cv1_TotalRecovered = cv1.sort_values('Total recovered', ascending=False)
+cv1_TotalCasesPerMillion = cv1.sort_values('Total cases per 1M pop.', ascending=False)
+cv1_TotalDeaths = cv1.sort_values('Total deaths', ascending=False)
+cv1_NewCases = cv1.sort_values('New cases', ascending=False)
+cv1_NewDeaths = cv1.sort_values('New deaths', ascending=False)
+
+#Graph of SAARC 'Total cases'
+x = cv1_TotalCases.head(7)
+rowcategory = "Country"
+columncategory = "Total cases"
+figureNum = 7
+graphColor = "Red"
+titlestring = "SAARC countries compare based on "
+plotGraph(x, rowcategory, columncategory, figureNum, graphColor, titlestring)
+
+#Graph of top SAARC 'Total recovered'
+x = cv1_TotalRecovered.head(7)
+rowcategory = "Country"
+columncategory = "Total recovered"
+figureNum = 8
+graphColor = "lime"
+titlestring = "SAARC countries compare based on "
+plotGraph(x, rowcategory, columncategory, figureNum, graphColor, titlestring)
+
+#Graph of top SAARC 'Total cases per 1M pop.'
+x = cv1_TotalCasesPerMillion.head(7)
+rowcategory = "Country"
+columncategory = "Total cases per 1M pop."
+figureNum = 9
+graphColor = "aqua"
+titlestring = "SAARC countries compare based on "
+plotGraph(x, rowcategory, columncategory, figureNum, graphColor, titlestring)
+
+#Graph of top SAARC 'Total deaths'
+x = cv1_TotalDeaths.head(7)
+rowcategory = "Country"
+columncategory = "Total deaths"
+figureNum = 10
+graphColor = "black"
+titlestring = "SAARC countries compare based on "
+plotGraph(x, rowcategory, columncategory, figureNum, graphColor, titlestring)
+
+#Graph of top SAARC 'New cases'
+x = cv1_NewCases.head(7)
+rowcategory = "Country"
+columncategory = "New cases"
+figureNum = 11
+graphColor = "orangered"
+titlestring = "SAARC countries compare based on "
+plotGraph(x, rowcategory, columncategory, figureNum, graphColor, titlestring)
+
+#Graph of top SAARC 'New deaths'
+x = cv1_NewDeaths.head(7)
+rowcategory = "Country"
+columncategory = "New deaths"
+figureNum = 12
+graphColor = "gray"
+titlestring = "SAARC countries compare based on "
+plotGraph(x, rowcategory, columncategory, figureNum, graphColor, titlestring)
