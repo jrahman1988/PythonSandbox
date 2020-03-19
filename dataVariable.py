1f0cd11949df5d1d7582d3837dbae1de3b4e6d22
jamil <jrahman88>
2020-03-19 04:51:32 -0400

    Newly added

--- dataVariable.py
+++ dataVariable.py
@@ -5,8 +5,11 @@ var1 = 100
 var2 = 100.99
 var3 = True
 var4 = 5+4j
+var5 = "Hello"
 
 print("Type of var1 = ", type(var1))
 print("Type of var2 = ", type(var2))
 print("Type of var3 = ", type(var3))
 print("Type of var4 = ", type(var4))
+print("Type of var5 = ", type(var5))
+print("Here is a string {}".format(var5))
