aa8daf9d857c23f81d4477c6dbae07f7369518e5
jamil <jrahman88>
2020-04-09 02:23:08 -0400

    Newly added

--- myWebScrapping5.py
+++ myWebScrapping5.py
@@ -1,22 +1,44 @@
 '''
 Webscraping to GET the html response, then PARSE and fetch the content, FIND the tables, CONSTRUCT a DF
-We'll use here the pandas.read_html(), which returns a list of data frames. According to the PANDAS doc:
-"This function searches for <table> elements and only for <tr> and <th> rows and <td> elements within
-each <tr> or <th> element in the table. <td> stands for “table data”.
-Document URL: https://pandas.pydata.org/pandas-docs/version/0.23.4/generated/pandas.read_html.html
+Reference document URL: https://pandas.pydata.org/pandas-docs/version/0.23.4/generated/pandas.read_html.html
 '''
 import pandas as pd
 import requests
-
-# GET the response from the web page using requests library
-# req = requests.get("https://www.worldometers.info/coronavirus/")
+from matplotlib import pyplot as plt
+import datetime
+#
+#Format the date time to present on the graph
+dt = datetime.datetime.now()
+today = dt.strftime("%A, %d %B %Y, %H:%M:%S")
+#
+#GET the response from the web page using requests library
 req = requests.get("https://www.eia.gov/opendata/qb.php?category=2134732&sdid=INTL.36-12-BGD-BKWH.A")
 df = pd.read_html(req.content)[0]
 #
-print(type(df))
-print(df.head(10))
-print(df.info())
-# <class 'pandas.core.frame.DataFrame'>
+#Convert the generated values from billion to mega
+df['Value'] = df['Value'].multiply(1000)
+#
+#Modify the existing string 'billion kilowatthours' string to 'Million kWH' of 'Units' column
+#--here we take one column which is a seris, then apply the Series.str.replace(self, pat, repl, n=-1, case=None, flags=0, regex=True)--
+df['Units'] = df['Units'].str.replace('billion kilowatthours', 'Million kWH')
 
+#Fill all NaN values with 0
+df = df.fillna(0)
+#
+#Rename the columns
+df = df.rename(columns={'Period':'Year', 'Value': 'Generated amount in MkWh', 'Units': 'Million kWH'})
+#
+#Now read the file and plot the line graph
+fig, ax = plt.subplots()
+ax= df.plot(kind='line', figsize=(10,5), x='Year', y='Generated amount in MkWh', grid=True, title="Yearly trend of solar power generation in Bangladesh", ax=ax)
+ax.set_xlabel("Period: 1980 to 2018")
+ax.set_ylabel("Trillion kWH")
+plt.suptitle(today)
+plt.show()
+#
+#Write to a CSV file
 df.to_csv("~/Desktop/Learning/Sandbox/PythonSandbox/Data/BDSolarPowerGeneration.csv", index=False)
 
+print(df.info())
+print(df['Generated amount in MkWh'].head())
+print(df['Million kWH'].head())
