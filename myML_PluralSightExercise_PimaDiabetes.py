167f77b0ade00dff1f761ff06d027f88c346e167
jamil <jrahman88>
2020-04-25 03:40:50 -0400

    Newly added

--- myML_PluralSightExercise_PimaDiabetes.py
+++ myML_PluralSightExercise_PimaDiabetes.py
@@ -30,6 +30,8 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
 from sklearn.naive_bayes import GaussianNB
+from sklearn import metrics
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.tree import export_graphviz
 import pydot
@@ -165,143 +167,70 @@ X_test = fill_0.fit_transform(X_test)
 
 '''
 TRAINING OF NAIVE BAYES MODEL:
-1. We'll use GaussianNB class of sklearn.naive_bayes and train with X_train U_train data
+1. We'll use GaussianNB class of sklearn.naive_bayes and train with X_train y_train data
 '''
 nb_model = GaussianNB()
 nb_model.fit(X_train, y_train.ravel())
 
-#---> ekhan theke
+'''
+PERFORMANCE ON TRAINING DATA USING NAIVE BAYES MODEL:
+1. Predict the value with X_train data
+2. We need to check the prediction accuracy using training data on the Naive Bayes model
+'''
+nb_predict_train = nb_model.predict(X_train)
+predict_performance_train = metrics.accuracy_score(y_train, nb_predict_train)
+print("Accuracy of prediction with training data {0:.4f}".format(predict_performance_train))
 
-# # #Data integrity check by data visualiztion
-# # x_day = features['day']
-# #
-# # y_Actual = features['actual']
-# # y_Average = features['average']
-# # y_Previousday = features['temp_1']
-# # y_Daybeforeyesterday = features['temp_2']
-#
-# # #Plot a bar graph from the data (names, runs)
-# # plt.figure(figsize=(20, 5))
-# # plt.title("Data Integrity Check")
-# # plt.xlabel("Days")
-# # plt.ylabel("Temperature")
-# # plt.bar(x_day,y_Average, color="Green")
-# # plt.grid(True)
-# # plt.show()
-#
-# #Data cleaning and setup
-# #
-# #Repalce any NaN values to 0 in the dataset
-# features = features.fillna(0)
-#
-# # One-hot encode the data using pandas get_dummies
-# features = pd.get_dummies(features, dummy_na=False)
-#
-# #Display the first 5 rows of the last 12 columns print using pd.iloc[:,5:] where it will consider all rows, and columns starting at 5 to end
-# # print(features.iloc[:,5:].head(5))
-# # features.to_csv('~/Desktop/Learning/DataScience/ML Exercise/temps_after.csv')
-# # print(features['actual'])
-# ''''''
-# # Labels are the values are kep under 'actual' column we want to predict, which is 'y'
-# labels = features['actual']
-# ''''''
-# # Remove the label column ('actual' column) from the dataframe, we will use full df except 'actual' column as features
-# features= features.drop(['actual'], axis = 1)
-# # print(features)
-# # features.to_csv('~/Desktop/Learning/DataScience/ML Exercise/temps_after.csv')
-# # ''''''
-# # Saving feature names (minus 'actual') for later use, which is 'X'
-# feature_list = list(features.columns)
-# # ''''''
-# # # Convert to numpy array
-# # features = np.array(features)
-# # # print(labels)
-# '''
-# TRAINING & TESTING SETS
-# The following code splits the data sets with another single line:
-# Using Skicit-learn to split data into training and testing sets
-# We can also use df instead of numpy array, for more information refer to API doc: https://scikit-learn.org/stable/index.html
-# '''
-# # Split the data into training and testing sets
-# train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size = 0.25, random_state = 42)
-# #
-# #Sanity check of the shape of the data
-# print('Training feature data (X-training) shape = ', train_features.shape)
-# print('Training labels data (y-training) shape = ',train_labels.shape)
-# print('Test feature data (X-test) shape = ',test_features.shape)
-# print('Test labels data (y-test) shape = ',test_labels.shape)
-# # print(test_features.info())
-#
-# '''
-# ESTABLISH BASELINE:
-# '''
-# # The baseline predictions are the historical averages
-# baseline_preds = test_features['average']
-# print('Baseline prediction shape = ', baseline_preds.shape)
-# # print('Baseline Prediction :', baseline_preds)
-#
-# # Baseline errors, and display average baseline error
-# baseline_errors = abs(baseline_preds - test_labels)
-# print("The baseline average error (BAE): {} ".format(round(baseline_errors.mean(axis=0),2)))
-#
-# '''
-# TRAIN MODEL:
-# After all the work of data preparation, creating and training the model is pretty simple using Scikit-learn.
-# We import the random forest regression model from skicit-learn, instantiate the model, and fit
-# '''
-# # Instantiate model with 1000 decision trees
-# rf = RandomForestRegressor(n_estimators = 1000, random_state = 42)
-# # print(rf)
-#
-# # Train the model on training data
-# rf.fit(train_features, train_labels)
-#
-# '''
-# MAKE PREDICTION ON THE TEST DATA:
-# Our model has now been trained to learn the relationships between the features and the targets.
-# The next step is figuring out how good the model is! To do this we make predictions on the test features (the model is
-# never allowed to see the test answers). We then compare the predictions to the known answers. When performing
-# regression, we need to make sure to use the absolute error because we expect some of our answers to be low and some to
-# be high. We are interested in how far away our average prediction is from the actual value so we take the absolute value
-# (as we also did when establishing the baseline).
-# '''
-# # Use the RF's predict method on the test data
-# predictions = rf.predict(test_features)
-# # print(test_features)
-# # print(predictions)
-#
-# # Calculate the absolute errors
-# errors = abs(predictions - test_labels)
-# # print(errors)
-#
-# # Print out the mean absolute error (mae)
-# print('Mean Absolute Error (MAE) : {}'.format(round(errors.mean(axis=0),2)))
-#
-# '''
-# DETERMINE PERFORMANCE METRICS:
-# To put our predictions in perspective, we can calculate an accuracy using
-# the mean average percentage error (MAPE) subtracted from 100 %
-# '''
-# # Calculate mean absolute percentage error (MAPE)
-# mape = 100 * (errors / test_labels)
-# accuracy = 100 - np.mean(mape)
-# print('Accuracy of this model (100% - MAPE) :', round(accuracy, 2), '%.')
-#
-# '''
-# INTERPRETATION OF 'RAIN FOREST' MODEL:
-# The question is: how does this model arrive at the values?
-# '''
-# #Visualizing a Single Decision Tree (the following code will create tree of 15 levels)
-# # tree = rf.estimators_[5]
-# # export_graphviz(tree, out_file = 'tree.dot', feature_names = feature_list, rounded = True, precision = 1)
-# # (graph, ) = pydot.graph_from_dot_file('tree.dot')
-# # graph.write_png('tree.png')
-#
-# #Rather create a small layered lree (3 layers)
-# # Limit depth of tree to 3 levels
-# rf_small = RandomForestRegressor(n_estimators=10, max_depth = 3)
-# rf_small.fit(train_features, train_labels)# Extract the small tree
-# tree_small = rf_small.estimators_[5]# Save the tree as a png image
-# export_graphviz(tree_small, out_file = 'small_tree.dot', feature_names = feature_list, rounded = True, precision = 1)
-# (graph, ) = pydot.graph_from_dot_file('small_tree.dot')
-# graph.write_png('small_tree.png')
+'''
+PERFORMANCE ON TESTING DATA USING NAIVE BAYES MODEL:
+1. Predict the value with X_test data
+2. We need to check the prediction accuracy using testing data on the Naive Bayes model
+'''
+nb_predict_test = nb_model.predict(X_test)
+predict_performance_test = metrics.accuracy_score(y_test, nb_predict_test)
+print("Accuracy of prediction with testing data {0:.4f}".format(predict_performance_test))
+
+'''
+PERFORMANCE STATISTICS MEASURE BY CONFUSION MATRIX of y_test and nb_predict_test data:
+1. Using confusion_matrix() method of metrics class from sklearn library
+2. Confusion matrix is to evaluate the accuracy of a classification
+3. By definition a confusion matrix 'C' is such that 'Cij' is equal to the number of observations known to be in group
+   'i' and predicted to be in group 'j'.
+4. Thus in binary classification, the count of true negatives (TN) is 'C00', false negatives (FN) is 'C10',
+    true positives (TP) is 'C11' and false positives (FP) is 'C01':
+   Predicted False  Predicted True
+   c00 (TN)         c01 (FP) (row = actual fasle)
+   c10 (FN)         c11 (TP) (row = actual true)
+5. In a perfect classifier, the matrics will be:
+   c01 (FP) and c10 (FN) should be '0'
+'''
+print("Confusion Matrix")
+print("{}".format(metrics.confusion_matrix(y_test, nb_predict_test)))
+print("")
+
+print("Classification Report")
+print(metrics.classification_report(y_test, nb_predict_test))
+
+'''
+RANDOM FOREST MODEL TO IMPROVE THE PREDICTION:
+1. If the Confusion matrix shows the naive bayes model needs improvement, we can try to see how Random Forest works
+2. We create a RF model and train with X_train and y_train data
+2. Note that, we use np.ravel() format for y_train to flatten the array
+'''
+rf_model = RandomForestClassifier(random_state=42)
+rf_model.fit(X_train, y_train.ravel())
+
+rf_predict_train = rf_model.predict(X_train)
+print("Training data accuracy with Random Forest model: {0:.4f}".format(metrics.accuracy_score(y_train, rf_predict_train)))
+
+rf_predict_test = rf_model.predict(X_test)
+print("Test data accuracy with Random Forest model: {0:.4f}".format(metrics.accuracy_score(y_test, rf_predict_test)))
+
+print("Confusion Matrix with RF model")
+print("{}".format(metrics.confusion_matrix(y_test, rf_predict_test)))
+print("")
+
+print("Classification Report with RF model")
+print(metrics.classification_report(y_test, rf_predict_test))
+
+
